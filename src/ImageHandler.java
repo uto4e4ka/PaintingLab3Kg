d6afa04337dc1c629504e84be173be180e029814
Alexey Zhestkov <alezhestkov@gmail.com>
2024-11-20 21:33:34 +0400

    ll

--- src/ImageHandler.java
+++ src/ImageHandler.java
@@ -8,18 +8,20 @@ import java.io.IOException;
 public class ImageHandler extends JPanel {
     BufferedImage image;
    public ImageHandler() throws IOException {
-        image = ImageIO.read(new File(""));
+        image = ImageIO.read(new File("C:\\Users\\Alex\\Downloads\\img1_low_contrast.jpg"));
     }
 
+    public BufferedImage ge
     @Override
     protected void paintComponent(Graphics g) {
         super.paintComponent(g);
+        g.drawImage(image,0,0,this);
     }
     public static void main(String[]args) throws IOException {
         JFrame jFrame = new JFrame("s");
         ImageHandler imageHandler = new ImageHandler();
         jFrame.add(imageHandler);
-        jFrame.setSize(500,500);
+        jFrame.setSize(1000,1000);
         jFrame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
         jFrame.setVisible(true);
     }
